629c6b70e44761efa0c345a76619fb8b7fa8c708
InQ9Al3 <118564919+InQ9Al3@users.noreply.github.com>
2023-02-26 18:41:29 +0100

    commit err comment

--- server/routes/chat.js
+++ server/routes/chat.js
@@ -2,6 +2,7 @@ const express = require("express")
 const router = express.Router()
 const path = require("path")
 
+//////// Error here: Cannot POST /chat
 router.post("/chat", (req, res) => {
   res.status(200).sendFile(path.resolve(__dirname, "../../client/chat.html"))
 })
